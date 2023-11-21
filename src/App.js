653f441cf2236590d71e9e530ae42b0acbca0c61
Wojtek <github@wmarzec.eu>
2023-11-21 20:36:47 +0100

    takie tam testy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // Hook stanu do przechowywania elementów menu
+  const [menuItems, setMenuItems] = useState([]);
+
+  // Hook efektu do ładowania danych menu z pliku JSON przy pierwszym renderowaniu
+  useEffect(() => {
+    fetch('/json_static/menu.json') // Ścieżka do pliku JSON
+      .then(response => response.json()) // Parsowanie odpowiedzi jako JSON
+      .then(data => setMenuItems(data)) // Aktualizacja stanu menuItems danymi z JSON
+      .catch(error => console.error("Fetching menu failed:", error));
+  }, []); // Pusta tablica jako drugi argument oznacza, że efekt uruchomi się tylko raz
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {menuItems.map((item, index) => (
+            // Tworzenie listy elementów menu
+            <li key={index}>
+              <a href={item.url} target="_blank" rel="noopener noreferrer">
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
