d4d8f617294756151e8901a98859619a3aa2439e
miloszxxx <zusu420@gmail.com>
2023-12-07 16:41:43 +0100

    dokumentaca dodana do pliku app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+/**
+ * Komponent główny aplikacji React.
+ * Komponent ten jest odpowiedzialny za renderowanie struktury aplikacji w oparciu o React Router.
+ * Zawiera nawigację oraz definiuje trasy dla poszczególnych komponentów.
+ *
+ * @component
+ * @return {JSX.Element} Element JSX reprezentujący strukturę aplikacji.
+ */
 import React from 'react';
 import {
   BrowserRouter as Router,
@@ -11,6 +19,12 @@ import ArticlePage from './components/ArticlePage/ArticlePage.js';
 import FormPage from './components/FormPage/FormPage.js'; // Dodano import FormPage
 import './App.css';
 
+/**
+ * Główny komponent aplikacji.
+ *
+ * @function
+ * @return {JSX.Element} Element JSX reprezentujący główny komponent aplikacji.
+ */
 function App() {
   return (
     <Router>
@@ -19,17 +33,24 @@ function App() {
         <div className="module-head">from App nav module</div>
         <ul>
           <nav>
+            {/* Link do strony głównej */}
             <Link className="cl-li" to="/"><li>Home</li></Link>
+            {/* Link do narzędzia do formatowania JSON */}
             <Link className="cl-li" to="/json-formatter"><li>PrettyJson</li></Link>
+            {/* Link do strony z ogłoszeniami */}
             <Link className="cl-li" to="/form"><li>Ogłoszenia</li></Link> {/* Dodano link do Ogłoszeń */}
           </nav>
         </ul>
 
         {/* Definicja tras */}
         <Routes>
+          {/* Trasa do wyświetlania pojedynczego artykułu */}
           <Route path="/article/:id" element={<ArticlePage />} />
+          {/* Trasa do strony z ogłoszeniami */}
           <Route path="/form" element={<FormPage />} /> {/* Dodano trasę do Ogłoszeń */}
+          {/* Domyślna trasa prowadząca do strony głównej */}
           <Route path="/" element={<HomePage />} />
+          {/* Trasa do narzędzia do formatowania JSON */}
           <Route path="/json-formatter" element={<PrettyJson />} />
         </Routes>
       </div>
